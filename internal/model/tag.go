6b7d293e3c71988630ff32a0d3650d14e1edf395
Koopa <koopapapa@gmail.com>
2021-12-27 00:07:59 +0800

    Koopa Tag Handle/gin

--- internal/model/tag.go
+++ internal/model/tag.go
@@ -1,6 +1,9 @@
 package model
 
-import "github.com/koopa0/blog-service/pkg/app"
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/koopa0/blog-service/pkg/app"
+)
 
 type Tag struct {
 	*Model
@@ -15,4 +18,48 @@ type TagSwagger struct {
 
 func (t Tag) TableName() string {
 	return "blog_tag"
+}
+
+func (t Tag) Count(db *gorm.DB) (int, error) {
+	var count int
+	if t.Name != "" {
+		db = db.Where("name = ?", t.Name)
+	}
+
+	db = db.Where("state = ?", t.State)
+	err := db.Model(&t).Where("is_del = ?", 0).Count(&count).Error
+	if err != nil {
+		return 0, err
+	}
+
+	return count, nil
+}
+
+func (t Tag) List(db *gorm.DB, pageOffset, pageSize int) ([]*Tag, error) {
+	var tags []*Tag
+	var err error
+	if pageOffset >= 0 && pageSize >= 0 {
+		db = db.Offset(pageOffset).Limit(pageSize)
+	}
+	if t.Name != "" {
+		db = db.Where("name = ?", t.Name)
+	}
+	db = db.Where("state = ?", t.State)
+	if err = db.Where("id_del = ?", 0).Find(&tags).Error; err != nil {
+		return nil, err
+	}
+	return tags, nil
+}
+
+func (t Tag) Create(db *gorm.DB) error {
+	return db.Create(&t).Error
+}
+
+func (t Tag) Update(db *gorm.DB) error {
+	db = db.Model(&Tag{}).Where("id = ? AND is_del = ?", t.ID, 0)
+	return db.Update(t).Error
+}
+
+func (t Tag) Delete(db *gorm.DB) error {
+	return db.Where("id = AND is_del = ?", t.Model.ID, 0).Delete(&t).Error
 }
