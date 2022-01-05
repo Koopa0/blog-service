24639e01b7c00d3737a0c626d03950af7d3e2e38
Koopa <koopapapa@gmail.com>
2022-01-05 22:08:13 +0800

    Koopa Tag Handle-v2/gin

---
+++ internal/dao/dao.go
@@ -0,0 +1,52 @@
+package dao
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/koopa0/blog-service/internal/model"
+	"github.com/koopa0/blog-service/pkg/app"
+)
+
+type Dao struct {
+	engine *gorm.DB
+}
+
+func New(engine *gorm.DB) *Dao {
+	return &Dao{engine: engine}
+}
+
+func (d *Dao) CountTag(name string, state uint8) (int, error) {
+	tag := model.Tag{Name: name, State: state}
+	return tag.Count(d.engine)
+}
+
+func (d *Dao) GetTagList(name string, state uint8, page, pageSize int) ([]*model.Tag, error) {
+	tag := model.Tag{Name: name, State: state}
+	pageOffset := app.GetPageOffset(page, pageSize)
+	return tag.List(d.engine, pageOffset, pageSize)
+}
+
+func (d *Dao) CreateTag(name string, state uint8, createdBy string) error {
+	tag := model.Tag{
+		Name:  name,
+		State: state,
+		Model: &model.Model{CreatedBy: createdBy},
+	}
+
+	return tag.Create(d.engine)
+}
+
+func (d *Dao) UpdateTag(id uint32, name string, state uint8, modifiedBy string) error {
+	tag := model.Tag{
+		Name:  name,
+		State: state,
+		Model: &model.Model{ID: id, ModifiedBy: modifiedBy},
+	}
+
+	return tag.Update(d.engine)
+}
+
+func (d *Dao) DeleteTag(id uint32) error {
+	tag := model.Tag{Model: &model.Model{ID: id}}
+
+	return tag.Delete(d.engine)
+}
--- internal/model/model.go
+++ internal/model/model.go
@@ -6,6 +6,7 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/koopa0/blog-service/global"
 	"github.com/koopa0/blog-service/pkg/setting"
+	"time"
 )
 
 type Model struct {
@@ -36,8 +37,74 @@ func NewDBEngine(databaseSetting *setting.DatabaseSettingS) (*gorm.DB, error) {
 	}
 
 	db.SingularTable(true)
+	db.Callback().Create().Replace("gorm:update_time_stamp",
+		updateTimeStampForCreateCallback)
+	db.Callback().Update().Replace("gorm:update_time_stamp",
+		updateTimeStampForUpdateCallback)
+	db.Callback().Delete().Replace("gorm:delete", deleteCallback)
 	db.DB().SetMaxIdleConns(databaseSetting.MaxIdleConns)
 	db.DB().SetMaxOpenConns(databaseSetting.MaxOpenConns)
 
 	return db, nil
+}
+
+func updateTimeStampForCreateCallback(scope *gorm.Scope) {
+	if !scope.HasError() {
+		nowTime := time.Now().Unix()
+		if createTimeField, ok := scope.FieldByName("CreateOn"); ok {
+			if createTimeField.IsBlank {
+				_ = createTimeField.Set(nowTime)
+			}
+		}
+		if modifyTimeField, ok := scope.FieldByName("ModifiedOn"); ok {
+			if modifyTimeField.IsBlank {
+				_ = modifyTimeField.Set(nowTime)
+			}
+		}
+	}
+}
+
+func updateTimeStampForUpdateCallback(scope *gorm.Scope) {
+	if _, ok := scope.Get("gorm:update_column"); ok {
+		_ = scope.SetColumn("ModifiedOn", time.Now().Unix())
+	}
+}
+
+func deleteCallback(scope *gorm.Scope) {
+	if !scope.HasError() {
+		var extraOption string
+		if str, ok := scope.Get("gorm:delete_option"); ok {
+			extraOption = fmt.Sprint(str)
+		}
+
+		deletedOnField, hasDeleteOnField := scope.FieldByName("DeletedOn")
+		isDelField, hasIsDelField := scope.FieldByName("IsDel")
+		if !scope.Search.Unscoped && hasDeleteOnField && hasIsDelField {
+			now := time.Now().Unix()
+			scope.Raw(fmt.Sprintf(
+				"UPDATE %v SET %v=%v, %v=%v%v%v",
+				scope.QuotedTableName(),
+				scope.Quote(deletedOnField.DBName),
+				scope.AddToVars(now),
+				scope.Quote(isDelField.DBName),
+				scope.AddToVars(1),
+				addExtraSpaceIfExist(scope.CombinedConditionSql()),
+				addExtraSpaceIfExist(extraOption),
+			)).Exec()
+		} else {
+			scope.Raw(fmt.Sprintf(
+				"DELETE FROM %v%v%v",
+				scope.QuotedTableName(),
+				addExtraSpaceIfExist(scope.CombinedConditionSql()),
+				addExtraSpaceIfExist(extraOption),
+			)).Exec()
+		}
+	}
+}
+
+func addExtraSpaceIfExist(str string) string {
+	if str != "" {
+		return " " + str
+	}
+	return " "
 }
